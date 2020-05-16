01daa3dd1ee448b3a64a5d9c9b68ac39022de407
Kyle Johnston <johnstonkylew@gmail.com>
2020-05-16 13:48:49 -0400

    Use snake_case, format doc strings

--- graphdeps.py
+++ graphdeps.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-'graph dependencies in projects'
+"""Graph dependencies in projects"""
 import json
 from subprocess import Popen, PIPE
 import sys
@@ -65,19 +65,19 @@ valid_uuids = list()
 
 
 def call_taskwarrior(cmd):
-    'call taskwarrior, returning output and error'
+    """Call taskwarrior, returning output and error"""
     tw = Popen(['task'] + cmd.split(), stdout=PIPE, stderr=PIPE)
     return tw.communicate()
 
 
 def get_json(query_parsed):
-    'call taskwarrior, returning objects from json'
+    """Call taskwarrior, returning objects from json"""
     result, err = call_taskwarrior('export %s rc.json.array=on rc.verbose=nothing' % query_parsed)
     return json.loads(result)
 
 
 def call_dot(instr):
-    'call dot, returning stdout and stdout'
+    """Call dot, returning stdout and stdout"""
     dot = Popen('dot -T png'.split(), stdout=PIPE, stderr=PIPE, stdin=PIPE)
     return dot.communicate(instr)
 
@@ -107,12 +107,12 @@ if __name__ == '__main__':
                 prefix = datum['id']
                 if not datum.get('depends', ''): color = COLOR_UNBLOCKED
                 else:
-                    hasPendingDeps = 0
+                    has_pending_deps = 0
                     for depend in datum['depends'].split(','):
                         for datum2 in data:
                             if datum2['uuid'] == depend and datum2['status'] == 'pending':
-                                hasPendingDeps = 1
-                    if hasPendingDeps == 1: color = COLOR_BLOCKED
+                                has_pending_deps = 1
+                    if has_pending_deps == 1: color = COLOR_BLOCKED
                     else: color = COLOR_UNBLOCKED
 
             elif datum['status'] == 'waiting':
@@ -132,9 +132,9 @@ if __name__ == '__main__':
                 color = COLOR_MAX_URGENCY
 
             label = ''
-            descriptionLines = textwrap.wrap(datum['description'], CHARS_PER_LINE)
-            for descLine in descriptionLines:
-                label += descLine+"\\n"
+            description_lines = textwrap.wrap(datum['description'], CHARS_PER_LINE)
+            for desc_line in description_lines:
+                label += desc_line + "\\n"
 
             lines.append('"%s"[shape=box][BORDER_WIDTH=%d][label="%s:%s"][fillcolor=%s][style=%s]' % (datum['uuid'], BORDER_WIDTH, prefix, label, color, style))
             # documentation http://www.graphviz.org/doc/info/attrs.html
